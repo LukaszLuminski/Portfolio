8891c36de68147a4de68aad7e21cd23659171bb4
Lukasz Luminski <lukk87@gmail.com>
2020-01-28 20:13:29 +0000

    still picture, skills and projects to be updated

--- js/script.js
+++ js/script.js
@@ -21,11 +21,13 @@ $(document).ready(function() {
 let resizeTimer;
 
 window.addEventListener("resize", () => {
+  if (window.innerWidth < 500) {
   document.body.classList.add("resize-animation-stopper");
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(() => {
     document.body.classList.remove("resize-animation-stopper");
   }, 400);
+  }
 });
 
 var $hamburger = $(".hamburger");
